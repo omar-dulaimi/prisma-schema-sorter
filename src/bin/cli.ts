92c6d2a47d9adf917c83e8b67d6e48f2821d8abd
omar-dulaimi <o.m.dulaimi@gmail.com>
2021-11-05 17:38:33 +0200

    feat(cli): implement cli
    now it can be used directly without the need to install it

---
+++ src/bin/cli.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import chalk from 'chalk';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import { sortPrismaSchema } from '../lib';
+
+yargs(hideBin(process.argv))
+  .command(
+    'sort',
+    'sort your prisma schema in ascending fashion',
+    (yargs) => {
+      return yargs.option('schema', {
+        describe: 'path to the prisma schema',
+        type: 'string',
+        demandOption: false,
+      });
+    },
+    async (argv) => {
+      let { schema } = argv;
+      if (!schema) {
+        schema = './prisma/schema.prisma';
+        console.log(
+          chalk.green.bold('Using default prisma path: ./prisma/schema.prisma')
+        );
+      }
+      await sortPrismaSchema(schema);
+    }
+  )
+  .parse();
