40ac7dd12143876121caadbf2b36e63673efac15
omar-dulaimi <o.m.dulaimi@gmail.com>
2025-08-23 01:12:31 +0300

    migrate eslint configuration to v9 format
    - Remove deprecated .eslintrc.json
    - Add new eslint.config.js using flat config format
    - Update parser options and globals for Node.js environment
    - Fix compatibility with TypeScript ESLint v8+
    - Apply prettier formatting to affected source files

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,5 +2,5 @@ import { PrismaSchemaSectionType } from '../types';
 
 export const ascendingSorter = (
   a: PrismaSchemaSectionType,
-  b: PrismaSchemaSectionType
+  b: PrismaSchemaSectionType,
 ) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
